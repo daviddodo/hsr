c2afae27b4fd0430b63fb3148dab93107d08c6cd
daviddodo <hanhduy11@hotmail.com>
2025-08-14 19:46:42 -0400

    lint fixed?

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
-import app from "./app";
+import app from './app';
 import { PORT } from './config';
 
 app.listen(PORT, () => {
-           console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
