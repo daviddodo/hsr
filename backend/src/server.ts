4aa1a9effa8c6ec0bb438528af502c5ce18c9892
daviddodo <hanhduy11@hotmail.com>
2025-08-15 14:29:07 -0400

    final fix? no fix needed after all

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,5 @@
-import app from "./app";
-import { PORT } from "./config";
+import app from './app';
+import { PORT } from './config';
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
