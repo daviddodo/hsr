b86cf127243a585318e6df16ade6518d3fb15b44
daviddodo <hanhduy11@hotmail.com>
2025-08-14 20:10:54 -0400

    test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,5 +2,5 @@ import app from "./app";
 import { PORT } from "./config";
 
 app.listen(PORT, () => {
-     console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`);
 });
